c6e5c8fa81ec1579a3eef03b395e4aa70d8ed4bc
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-11-12 01:52:11 +0900

    予期しないエラー(ボタンを押した時に全てがリセットされる)の修正

--- Class/rep/sample.js
+++ Class/rep/sample.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');
 const message = document.getElementById('message');
-const button = document.getElementById('button');
+// const button = document.getElementById('button');
 const ctx = canvas.getContext('2d');
 
 function drawClock() {
